eba4fc37342979c406712818a54ef77ccf02f4b1
Dipesh Maywade <dipeshmaywade@gmail.com>
2021-04-23 02:08:04 +0530

    feat: ensure the player gets to exact wining position 100

--- snakeAndLadder.js
+++ snakeAndLadder.js
@@ -7,22 +7,39 @@ while (position < 100) {
     let rollDice = (Math.floor(Math.random() * 10) % 6) + 1;
     let option = Math.floor(Math.random() * 10) % 3;
 
-    switch (option) {
-        case LADDER:
+    if (option==LADDER) {
+
+        if (position + rollDice == 100) {
+            const end = position+rollDice;
+            console.log("Player Got The Ladder with " + rollDice + " and move to: " + end);
+            console.log("**HURRREYY U WIN THE GAME**");
+            break;
+        }
+
+        else if (position + rollDice > 100) {
+            position = position - rollDice;
+        }
+
+        else if (position + rollDice < 100) {
             position = position + rollDice;
-            console.log(`player Got The Ladder with ${rollDice} and move to: ${position}`);
-            break;
-        case SNAKE:
-            if (position >= 0) {
-                position = position - rollDice
-                console.log(`player Got The Snake with ${rollDice} and move to: ${position}`);
-                break;
-            } else {
-                console.log(`player At Zero TRY AGAIN`)
-                position=0;
-            }
-            break;
-        default:
-            console.log("Player Is Not Playing Stay at Same Position: " + position);
+            console.log("player Got The Ladder with " + rollDice + " and move to: " + position);
+        }
+    }
+
+    else if (option==SNAKE) {
+
+        if (position >= 0) {
+            position = position - rollDice;
+            console.log("Player Got The Snake with " + rollDice + " And Move To: " + position);
+        }
+
+        else {
+            console.log("player at ZERO TRY AGAIN");
+            position = 0;
+        }
+    }
+
+    else {
+        console.log("Player is Not Playing Stay at Same Position: " + position);
     }
 }
